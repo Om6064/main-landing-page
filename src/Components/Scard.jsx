17e1adab5cb124977a0e5a3ebd5e565dc80ddb91
viragbafna <viragbafna111@gmail.com>
2025-05-15 15:49:51 +0530

    added some hover

--- src/Components/Scard.jsx
+++ src/Components/Scard.jsx
@@ -3,12 +3,12 @@
 const Scard = (props) => {
   return (
     
-      <div className="col-md-6 col-xl-4 col-xxl-3 d-flex justify-content-center align-items-center">
+      <div className="col-md-6  col-xl-4 col-xxl-3 d-flex justify-content-center align-items-center">
         <div className="text-center" style={{ width: '18rem' }}>
-          <div className="card-body d-flex align-items-center justify-content-center flex-column">
+          <div className="card-body  d-flex align-items-center justify-content-center flex-column">
             <div className='d-flex align-items-center justify-content-center'>
-              <h5 className="card-title  fs-1 bor-red d-flex bg-color text-main align-items-center justify-content-center">
-                <i className={`${props.icon}`}></i>
+              <h5 className="card-title hover-ani  fs-1 bor-red d-flex bg-color text-main align-items-center justify-content-center">
+                <i className={`hover-icon ${props.icon}`}></i>
               </h5>
             </div>
             <p className="card-text mt-3 fs-5 fw-semibold">{props.title}</p>
